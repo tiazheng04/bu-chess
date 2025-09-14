2d52a7bac10b902d8124026063f32768ff0bb003
Kai <kai@gem.health>
2025-09-14 13:48:06 -0400

    vh wv

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,13 +5,13 @@ function Footer() {
     <footer
       style={{
         backgroundColor: "#f5f5f5",
-        padding: "2rem",
+        padding: "4.5vh",
         textAlign: "center",
         borderTop: "1px solid red",
       }}
     >
-      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "0.5rem", marginTop: "1rem" }}>
-        <div style={{ display: "flex", alignItems: "center", gap: "0.5rem" }}>
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "1.125vh", marginTop: "1.9vh" }}>
+        <div style={{ display: "flex", alignItems: "center", gap: "1.125vh" }}>
           <a
             href="https://www.instagram.com/buchessclub/"
             style={{
@@ -25,10 +25,10 @@ function Footer() {
             onMouseOver={e => (e.currentTarget.style.color = "#d32f2f")}
             onMouseOut={e => (e.currentTarget.style.color = "#ff6666")}
           >
-            <img src={instaLogo} alt="Instagram" style={{ width: "22px", height: "22px", marginRight: "0.5rem" }} />
+            <img src={instaLogo} alt="Instagram" style={{ width: "22px", height: "22px", marginRight: "1.125vh" }} />
             Instagram
           </a>
-          <span style={{ marginLeft: "1.5rem" }}>bostonuniversitychessclub@gmail.com</span>
+          <span style={{ marginLeft: "3.35vw" }}>chessbu@bu.edu</span>
         </div>
         <div>
           <p style={{ margin: 0 }}>&copy; {new Date().getFullYear()} Boston University Chess Club. All rights reserved.</p>
